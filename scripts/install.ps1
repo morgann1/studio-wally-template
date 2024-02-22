e5c70f60e4c0cbea2a7ee94c119be81a50ce09a2
morgann1 <58310810+morgann1@users.noreply.github.com>
2024-02-22 08:01:30 +0000

    add stylua

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -4,8 +4,10 @@ rojo sourcemap -o sourcemap.json default.project.json
 
 if (Test-Path -Path "./Packages") {
     wally-package-types -s sourcemap.json Packages/
+    stylua Packages/
 }
 
 if (Test-Path -Path "./ServerPackages") {
     wally-package-types -s sourcemap.json ServerPackages/
+    stylua ServerPackages/
 }
