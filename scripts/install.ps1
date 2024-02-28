74476e238ddc31f83055aeaa83dcf07fb02d2d52
Morgan <58310810+morgann1@users.noreply.github.com>
2024-02-28 14:22:50 +0000

    woah echos

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,13 +1,32 @@
+echo "🚀 Running 'aftman install'"
 aftman install
+
+echo "🚀 Running 'wally install'"
 wally install
+
+echo "🚀 Generating Rojo sourcemap"
 rojo sourcemap -o sourcemap.json default.project.json
 
 if (Test-Path -Path "./Packages") {
+    echo "🚀 Generating types for Packages/ via wally-package-types"
     wally-package-types -s sourcemap.json Packages/
+
+    echo "🚀 Styling Packages/ via stylua"
     stylua Packages/
 }
 
 if (Test-Path -Path "./ServerPackages") {
+    echo "🚀 Generating types for ServerPackages/ via wally-package-types"
     wally-package-types -s sourcemap.json ServerPackages/
+
+    echo "🚀 Styling ServerPackages/ via stylua"
     stylua ServerPackages/
+}
+
+if (Test-Path -Path "./Packages/React.lua") {
+    echo "🚀 Editing Packages/React.lua to include _G.__DEV__"
+
+    $reactContents = "_G.__DEV__ = game:GetService('RunService'):IsStudio()`n"
+    $reactContents = $reactContents + (Get-Content -Path .\Packages\React.lua -Encoding ASCII -Raw)
+    Set-Content -Path .\Packages\React.lua -Value $reactContents -Encoding ASCII
 }
